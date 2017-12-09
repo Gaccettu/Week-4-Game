bf95db3864be628a529551ea7d7800a468c47aec
Guy Accettura <gaccettu@indiana.edu>
2017-12-09 09:53:33 -0600

    layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+//Define each character variable as an object with stats
+    //Stats defined are HP, Attack, and Counter Attack
+//Create an onclick action that selects the character
+//Move the other three characters to the enemies section
+//Select an enemy character to fight
+//Click the attack button
+    //Decrease HP of enemy character by attack strength
+    //Decrease HP of chosen character by enemy counter attack strength
+    //Increase Attack of chosen character
+    //Repeat until one character's HP is empty
+//Repeat actions until all enemies defeated or selected character dies
